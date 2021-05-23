8ddfab0e4ac2697ed00980f276b7186f631f3175
MoonVNN <tuando000.dthap@gmai.com>
2021-05-23 22:27:51 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -22,7 +22,9 @@ module.exports = {
 							"!normalqueue hoặc !nq - Xem hàng chờ.\n" +
 							"!prio hoặc !prioqueue - Xem thông số bao gồm hàng chờ. ưu tiên, trực tuyến.\n" +
 							"!online - Xem người chơi đang hoạt động.\n"+ 
-							"!status - Xem hàng chờ, ưu tiên, online."
+							"!status - Xem hàng chờ, ưu tiên, online.\n" +
+							"!avatar - Xem avatar.\n"
+							+ "!sudo - Cho bot chat nội dung yêu cầu. ( chỉ dev )"
 						)
 						.setColor('0x2EA711')
 						.setTimestamp()
