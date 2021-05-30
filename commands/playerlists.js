08e01ba0a6e8555115f816c4fd500ba237e527ec
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-05-30 18:19:42 +0700

    Update playerlists.js

--- commands/playerlists.js
+++ commands/playerlists.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { RichEmbed } = require('discord.js');
 var Scriptdb = require('script.db');
 
 module.exports = {
@@ -6,16 +6,18 @@ module.exports = {
     aliases: ['playerlist', 'pl'],
     
     async execute(client, message, args) {
-        var db = new Scriptdb('./data.json');
+        var db = new Scriptdb(client.config.disk + '/data.json');
 
         var playerArray = db.get('players');
+
+        if(playerArray == undefined) return message.channel.send("Bot chưa kết nối vào server hoặc server không hoạt động.")
 
         var count = playerArray.length;
 
         message.channel.send("Đang tính toán...").then(msg => {
             var ping = (msg.createdTimestamp - message.createdTimestamp);
 
-            var embed = new MessageEmbed()
+            var embed = new RichEmbed()
                             .setTitle("Player list")
                             .setDescription("Đã tải trong " + ping + "ms")
                             .addField('Trực tuyến', count, false)
