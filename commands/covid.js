883d8a62ecd5f7137880ab11cc66507e4feffdd6
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-06-10 12:29:24 +0700

    covid 19

---
+++ commands/covid.js
@@ -0,0 +1,21 @@
+const covid19 = require('covid19-stats');
+
+module.exports = {
+    name: "covid",
+    aliases: ['covid19', 'covid-19', 'coronavirus', 'corona'],
+    
+    async execute(client, message, args) {
+        let data = await covid19.getCountry('vietnam');
+
+        var totalCase = data.totalCases;
+        var totalCaseDeaths = data.totalDeaths;
+        var newCase = data.newCases;
+        var newDeaths = data.newDeaths;
+
+        message.channel.send({embed: {
+            title: '| COVID-19 |',
+            description: "**Việt Nam**\n\n**Số ca**: " + totalCase + "\n**Số ca mới**: " + newCase + "\n**Số ca tử vong**: " + totalCaseDeaths + "\n**Số ca tử vong mới**: " + newDeaths
+            ,color: 0x2EA711
+        }});
+    }
+}
