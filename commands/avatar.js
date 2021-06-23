720c920262308f2fb8efb159724fd6960a836ddd
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-06-23 10:40:01 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,11 +10,7 @@ module.exports = {
         if(isNaN(user) && !tag) user = message.author; 
         if(tag) user = tag.id;
 
-        if(user.id == "425599739837284362") return message.channel.send("huỷ yêu cầu avatar người này.");
-        
         client.fetchUser(user).then(user => {
-            if(user.id == "425599739837284362") return message.channel.send("huỷ yêu cầu avatar người này.");
-
             if(user == null || user == undefined) return message.channel.send("Không tìm thấy user").then(msg => msg.delete(60000));;
             if(user.avatarURL == undefined || user.avatarURL == null) return message.channel.send("Không có avatar").then(msg => msg.delete(60000));;
 
