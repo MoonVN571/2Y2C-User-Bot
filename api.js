662a3164ca421b3154c9f73a9dae08e81eb205b5
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-07-05 11:52:11 +0700

    Update api.js

--- api.js
+++ api.js
@@ -20,13 +20,14 @@ function API() {
         var mstr = d.split("/")[0];
         var ystr = d.split("/")[2];
 
-        if(dstr >= 1) dstr = dstr - 1;
-        if(mstr >= 1) mstr = mstr - 1;
+        if(dstr >= 1) dstr--;
+        if(mstr >= 1) mstr--;
         if(ystr == 1970) ystr = ystr - 1970;
         
         var hstr = parseInt(t.split(":")[0]);
         var minstr = parseInt(t.split(":")[1]);
 
+        if(t.split(" ")[1] == "PM") hstr = hstr + 12;
         if(d.split("/")[0] == 1) hstr = hstr - 8;
 
         log(dstr, mstr, ystr, hstr, minstr);
@@ -174,13 +175,15 @@ function API() {
 
         var d = new Date();
         var timenow = d.getTime();
-
         var ticks = timenow - uptime;
         var temp = ticks / 1000;
         var day = hours = 0, minutes = 0, seconds = 0;
         hours = parseInt(((temp - day * 86400) / 3600))
         minutes = parseInt(((temp - day * 86400 - hours * 3600)) / 60)
         seconds = parseInt(temp % 60)
+
+        if(hours > 24) return "0h 0m 0s";
+
         if(uptime === undefined) {
             hours = 0;
             minutes = 0;
